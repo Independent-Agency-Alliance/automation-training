94660f0ebebfb21887c3a610463b7b663d2b76a3
Micua3105 <lekieuhau12345@gmail.com>
2024-01-10 12:16:30 +0700

    create new file, test case pagination works correctly

--- src/02-ttts2023-agu-pagination-kieuhau.test.js
+++ src/02-ttts2023-agu-pagination-kieuhau.test.js
@@ -4,28 +4,77 @@ fixture("ttts2023-agu").page("https://www.agu.edu.vn/vi/tim-kiem");
 
 test("validate that the pgination works correctly", async (t) => {
   const searchInputSelector = Selector(".search .input-group .search");
-  const nextPage = Selector(".main .row .pagination .page-item:last-child .page-link");
-  const previousPage = Selector(".main .row .pagination .page-item:first-child .page-link");
+  const nextPageButton = Selector(".main .row .pagination .page-item:last-child .page-link");
+  const previousPageButton = Selector(".main .row .pagination .page-item:first-child .page-link");
   const currentPage = Selector('.main .row .pagination .active .page-link');
 
   await t.expect(searchInputSelector.exists).ok("input exsits");
   await t.typeText(searchInputSelector, "tuyển sinh").pressKey("enter");
 
-  await t.expect(nextPage.exists).ok("next exsits");
-  await t.expect(previousPage.exists).ok("previous exsits");
+  await t.expect(nextPageButton.exists).ok("next exsits");
+  await t.expect(previousPageButton.exists).ok("previous exsits");
   await t
     .expect(currentPage.exists).ok("current exsits")
     .expect(currentPage.innerText).eql("1");
   
 
-  //click nextPage
+  //click nextPageButton
   await t
-    .click(nextPage)
+    .click(nextPageButton)
     .expect(currentPage.innerText).eql("2");
  
-  //click previousPage
+  //click previousPageButton
   await t
-  .click(previousPage)
+  .click(previousPageButton)
   .expect(currentPage.innerText).eql("1");
 
+});
+
+
+
+//'.main .thong-tin .col-sm-4:first-child'
+
+test.only("validate that the pgination works correctly", async (t) => {
+  const searchInputSelector = Selector(".search .input-group .search");
+  const nextPageButton = Selector(".main .row .pagination .page-item:last-child .page-link");
+  const previousPageButton = Selector(".main .row .pagination .page-item:first-child .page-link");
+  const currentPageButton = Selector('.main .row .pagination .active .page-link');
+  const page2Button = Selector('.main .row .pagination .page-item:nth-child(3) .page-link');
+  const firstPostTitle = Selector('.main .thong-tin .col-sm-4:first-child');
+  const firstPostTitleContent1 = await firstPostTitle.textContent;
+
+  await t.expect(searchInputSelector.exists).ok("input exsits");
+  await t.typeText(searchInputSelector, "tuyển sinh").pressKey("enter");
+
+  await t.expect(nextPageButton.exists).ok("next exsits");
+  await t.expect(previousPageButton.exists).ok("previous exsits");
+  await t
+    .expect(currentPageButton.exists).ok("current exsits")
+    .expect(currentPageButton.innerText).eql("1");
+  
+
+  //click page 2, expect content not the same
+  await t 
+    .click(page2Button)
+    .expect(currentPageButton.innerText).eql("2");
+
+    const firstPostTitleContent2 = await firstPostTitle.textContent;
+
+  await t  
+    .expect(firstPostTitleContent1).notEql(firstPostTitleContent2);
+
+
+   //click previousPageButton
+  await t
+  .click(previousPageButton)
+  .expect(currentPageButton.innerText).eql("1")
+  .expect(firstPostTitleContent2).notEql(firstPostTitleContent1);
+
+
+  // //click nextPageButton
+  await t
+    .click(nextPageButton)
+    .expect(currentPageButton.innerText).eql("2")
+    .expect(firstPostTitleContent1).notEql(firstPostTitleContent2);
+ 
 });
