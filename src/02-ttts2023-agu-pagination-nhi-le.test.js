fc4dd708aa806f7e45c155273a8a49d104a819b2
NhuyNhuy63 <lethingocnhi632@gmail.com>
2024-01-12 00:24:04 +0700

    fix validate pagination

--- src/02-ttts2023-agu-pagination-nhi-le.test.js
+++ src/02-ttts2023-agu-pagination-nhi-le.test.js
@@ -14,7 +14,9 @@ test("search for 'tuyển sinh' and validate some results appear", async (t) => 
     const searchInputSelector = Selector(".search .input-group .search");
     const pagination = Selector(".main .page-item .page-link");
     const page2Button = Selector(".main .page-item .page-link").nth(2);
-    const firstPostTitle = Selector(".main .container .blog-details");
+    const nextPageButton = Selector(".main .page-item:last-child .page-link");
+    const previousPageButton = Selector(".main .page-item:first-child .page-link");
+    const firstPostTitle = Selector(".main .thong-tin .col-sm-4:first-child");
     const firstPostTitleContent = await firstPostTitle.textContent;
 
     await t.expect(searchInputSelector.exists).ok("Search box should exist");
@@ -27,8 +29,18 @@ test("search for 'tuyển sinh' and validate some results appear", async (t) => 
       .ok("At least one search result should exist");
     
     await t.expect(pagination.exists).ok("pagination should exist");
+
     await t.click(page2Button);
-    const firstPostTitleOnPage2 = Selector(".main .container .blog-details:first-child");
+    const firstPostTitleOnPage2 = Selector(".main .thong-tin .col-sm-4:first-child");
     const firstPostTitleOnPage2Content = await firstPostTitleOnPage2.textContent;
     await t.expect(firstPostTitleContent).notEql(firstPostTitleOnPage2Content);
+
+    await t .click(previousPageButton);
+    await t.expect(firstPostTitleContent).notEql(firstPostTitleOnPage2Content);
+  
+  
+    // //click nextPageButton
+    await t
+      .click(nextPageButton);
+      await t.expect(firstPostTitleContent).notEql(firstPostTitleOnPage2Content);
   });
