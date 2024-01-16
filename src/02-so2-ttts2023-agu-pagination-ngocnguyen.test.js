7ad655cdc4e1adfbddecf7072e902d16c5340e23
nthikimngoc <ngocnguyen44008@gmail.com>
2024-01-16 05:35:31 -0600

    edited message and validate that the pagination doesn't appear

--- src/02-so2-ttts2023-agu-pagination-ngocnguyen.test.js
+++ src/02-so2-ttts2023-agu-pagination-ngocnguyen.test.js
@@ -6,13 +6,18 @@ fixture("ttts2023-agu").page("https://www.agu.edu.vn/vi/tim-kiem");
 
 test("search for 'giới thiệu' and only 1 page of result, validate the result and pagination", async (t) => {
     const searchInputSelector = Selector(".search .input-group .search");
-       
+    const currentPageButton = Selector('.main .row .pagination .active .page-link'); 
+
+    //validate that the pagination doesn't appear.
+    await t
+        .expect(currentPageButton.exists).ok("Current page exists")
+        .expect(currentPageButton.innerText).eql("1");
+
+    //search for "giới thiệu" and validate have 1 page of result
     await t.expect(searchInputSelector.exists).ok("Search box should exist");
-  
     await t.typeText(searchInputSelector, "giới thiệu").pressKey("enter");
-  
+    
     await t
       .expect(Selector(".main .thong-tin .blog-details .btn").exists)
-      .ok("At more six search result should exist");
-       
+      .ok("A page result should exist");
   });
